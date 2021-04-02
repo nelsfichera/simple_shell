ec0615be368a56b8bd6acab2c511746c5dc393f8
tieje <toj320@gmail.com>
2021-04-02 09:43:35 -0400

    shell_main.c is going to be a very small file that calls an important function in a while loop.

--- shell_main.c
+++ shell_main.c
@@ -1,11 +1,15 @@
 #include "holberton.h"
 
 /**
- * main - where everything begins
+ * main - where everything begins. It's better to have many small files dedicated to for each function than a few large files.
  * Return: 0
  */
 
 int main(void)
 {
-    /* getline() to start the function */
+    while (/*condition infinite condition*/)
+    {
+        /* getline() to start to get input */
+    }
+    return (0);
 }
