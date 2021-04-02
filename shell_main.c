0af9b05f717745fa15fbfd3aa5152b8c5f2beef6
tieje <toj320@gmail.com>
2021-04-02 11:00:50 -0400

    updated general designs

--- shell_main.c
+++ shell_main.c
@@ -7,7 +7,14 @@
 
 int main(void)
 {
-    /* create_process */
-    /* A function that starts child processes and waits until child process completes*/
+    /* getpid() info */
+    /* fork() */
+    /* if a process is a child, run execute() */
+        /* execute() */
+           /* A function that takes string input, parses it, and executes it.*/
+
+    /* else (if) a process is a parent */
+        /* run execve(./shell) to restart the shell */
+        /* if restarting the shell fails, print error and return (0); */
     return (0);
 }
