b6669794e61df8c02a1dec8237ff89eea17f808f
Nels <nelsfichera@gmail.com>
2021-04-05 15:38:37 -0400

    update header with str functions

--- holberton.h
+++ holberton.h
@@ -1,15 +1,30 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+/*standard lib headers*/
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-
+#include <stddef.h>
+/*macros and extern*/
 #define BUFFER_SIZE 1024
 #define DELIM "\n\a\t"
 extern char **environ;
+/*str handlers*/
 int _putchar(char c);
-
+int _strlen(char *s);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+char *_strtok(char *str, char *separator, int flag);
+/*main functions*/
+int execute(char **commands);
+char **parse(char *input, char *separator);
+/*builtins*/
+char *pathmod(char *compath);
+char *_env(const char *name, char **env);
+int exitshell(void);
 #endif
