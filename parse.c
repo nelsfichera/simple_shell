95ef85460eb69a6c98633049ff0f95e7cef83fba
Nels <nelsfichera@gmail.com>
2021-04-06 13:28:17 -0400

    Buffer size redefine, add missing parenthesis

--- parse.c
+++ parse.c
@@ -12,8 +12,7 @@ char **parse(char *input, char *separator)
 	if (input == NULL)
 		return (NULL);
 	
-	commands = malloc (buffer * sizeof(char*)); 
-	/*buffer size  should probably be defined in a macro?*/ 
+	commands = malloc(BUFFER_SIZE* sizeof(char*));  
 	
 	if (commands == NULL)
 	{
@@ -26,7 +25,7 @@ char **parse(char *input, char *separator)
 	{
 		commands[index] = parsed_line;
 		index++;
-		parsed_line(strtok(NULL, separator);
+		parsed_line = (strtok(NULL, separator));
 	}
 	commands[index] = NULL;
 	return(commands);
