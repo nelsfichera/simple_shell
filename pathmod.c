256d180f15899aa8556e3b4a44f97297a635a7f1
tieje <toj320@gmail.com>
2021-04-06 14:58:49 -0400

    deleted ispath from pathmod

--- pathmod.c
+++ pathmod.c
@@ -1,35 +1,3 @@
-#include "holberton.h"
-
-/**
- * isPath - determines if the command is a path
- * @isthispath: input string
- * Return: either 0 or 1. 1 for true. 0 for false.
- */
-int isPath(char *isthispath)
-{
-    int iter = 0;
-    int str_len = _strlen(isthispath);
-
-    if (str_len == 1 && isthispath[0] == '/')
-    {
-        return (1);
-    }
-    while (isthispath[iter])
-    {
-        if (isthispath[iter] != '/')
-        {
-            if (iter + 1 < str_len)
-            {
-                if (isthispath[iter + 1] == '/')
-                {
-                    return (1);
-                }
-            }
-        }
-    }
-    return (0);
-}
-
 /**
  * pathmod - path module that checks where the command comes from
  * @compath: command path. All commands are a path until proven guilty.
