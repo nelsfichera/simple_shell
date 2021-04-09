9cff84913579e7ae70bc764b3d0b7c7619403c7d
Nels <nelsfichera@gmail.com>
2021-04-09 14:45:56 -0400

    Fix seg fault with test function

--- _strcat.c
+++ _strcat.c
@@ -1,18 +1,30 @@
 #include "holberton.h"
 
-char *_strcat(char *dest, char *src)
+char *_strcat(char *str1, char *str2)
 {
 	int x, n;
-	int boolean = 1;
+	char *str3;
 
-	for (x = 0; boolean == 1; x++)
-	{
-		if (*(dest + x) == '\0')
-		{
-			for (n = 0; *(src + n) != '\0'; x++, n++)
-				*(dest + x) = '\0';
-			boolean = 0;
-		}
-	}
-	return (dest);
+	str3 = malloc(sizeof(char) * (_strlen(str1) + _strlen(str2) + 1));
+
+	if (str3 == NULL)
+		return (NULL);
+
+	for(x = 0; str1[x] != '\0'; x++)
+		new[x] = str1[x];
+	for(n = 0; str2[n] != '\0'; n++, x++)
+		str3[x] = str2[n];
+	str3 = '\0';
+
+	return(str3);
+
 }
+int main(void)
+{
+	strcpy(str1, "This is a string");
+	strcpy(str2, "This is another string");
+
+	_strcat(str1,str2);
+	return(0);
+}
+
