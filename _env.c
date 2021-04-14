4a0189369a24efafd66785a9fa1130eddc153b10
Nels <nelsfichera@gmail.com>
2021-04-14 17:14:31 -0400

    Big changes

--- _env.c
+++ _env.c
@@ -1,98 +1,46 @@
-#include "holberton.h"
 /**
-* _env - prints env variables by calling printenv
-* @env: the command env
+* _getenv - gets the environment variables from path
+* @var: the name of the variable
+* Return: variable on success, null on failure
+*/
+char *_getenv(const char *var)
+{
+	int var_len = _strlen(var);
+
+	char **ptr;
+	char *output;
+
+	ptr = environ;
+
+	while (*ptr)
+	{
+		if(_strcmp(*ptr, name) == 0)
+		{
+			output = *ptr + len + 1;
+			return (output);
+		}
+		ptr++;
+	}
+	return (NULL);
+}
+/**
+* _env - prints all the env variables
+* @env_var: environment variables
 * Return: 1
 */
-int _env(char **env)
+
+int _env(char **env_var)
 {
-	if (env)
-		_printenv(environ);
-	return (1);
-}
-/**
-* _printenv - prints environment variables
-* @env: the command env
-* Return: success on exit
-*/
-int _printenv(char **env)
-{
-	unsigned int index = 0;
+	unsigned int index;
 	int env_var_len = 0;
 
-	for (index = 0; env[index] != NULL; index++)
+	for (index = 0; env_var[index] != NULL; index++)
 	{
 		env_var_len = _strlen(env[index]);
-		write(1, env[index], env_var_len);
+		write(1, env_var[index], env_var_len);
 		write(1, "\n", 1);
 	}
-	return (EXIT_SUCCESS);
+	return (1);
 }
-/**
-* _getenv - gets the environment variables
-* @name: name of the variables
-* @env: the command env
-* Return: tokens of env variables
-*/
-char *_getenv(char *name, char **env)
-{
-	char *token1 = NULL, *token2 = NULL;
-	char *env_name = NULL, *current_env = NULL;
-	int x = 0;
+/*a set env will go here if we need to*/
 
-	env_name = strdup(name);
-
-	while (env[x] && env)
-	{
-		current_env = NULL;
-		token2 = NULL;
-		current_env = strdup(env[x]);
-		token1 = strtok(current_env, "=");
-		token2 = strdup(strtok(NULL, "="));
-		if (_strcmp(env_name, token1) == 0)
-			break;
-		free(current_env);
-		free(token2);
-		x++;
-	}
-	free(current_env);
-	free(env_name);
-	return (token2);
-}
-/**
-* _setenv - sets the environmental variables
-* @arg1: command being passed
-* @arg2: command being passed
-* @newenv: array of new environmental variables
-* Return: void
-*/
-void _setenv(char *arg1, char *arg2, char *newenv[])
-{
-	char *new_var, set_var[BUFFER_SIZE];
-	short x = 0, n = 0;
-
-	if (!arg1 || !arg2)
-	{
-		write(STDERR_FILENO, "Parameters not found\n", 29);
-		return;
-	}
-	_strcpy(set_var, arg1);
-	_strcat(set_var, "=");
-
-	while (environ[x] && _strcmp(environ[x], set_var))
-		x++;
-	_strcat(set_var, arg2);
-	if (environ[x])
-		_strcpy(environ[x], set_var);
-	else
-	{
-		new_var = _calloc(150, sizeof(char));
-		_strcpy(new_var, set_var);
-		while (newenv[n])
-			n++;
-		newenv[n] = new_var;
-		newenv[n + 1] = NULL;
-		environ[x++] = new_var;
-		environ[x] = NULL;
-	}
-}
---
+++ splitline.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+* splitline - splits a line into tokens
+* @input: the input line
+* Return: address of substrings
+*/
+char **splitline(char *input)
+{
+	char *token, **tokens;
+	int index = 0;
+	const char delim[] = (" \n\r\t\a");
+
+	tokens = _calloc (98, sizeof(char *));
+
+	if (!tokens)
+		return (NULL);
+
+	token = strtok(input, delim);
+	
+	while (token)
+	{
+		tokens[index] = token;
+		token = strtok(NULL, delim);
+		index++;
+	}
+	tokens[index];
+	return(tokens);
+
+}
+
+/*this may be where the new strtok goes if I have time*/
